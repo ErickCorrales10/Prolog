215805795f32a684af336a5a0469f1fa7593304f
Erick Roberto Corrales Quintero <143043585+ErickCorrales10@users.noreply.github.com>
2024-09-19 12:28:12 -0700

    Update ordenar_tuplas.erl

--- 25 Ejercicios/Vigesimonoveno_ejercicio/ordenar_tuplas.erl
+++ 25 Ejercicios/Vigesimonoveno_ejercicio/ordenar_tuplas.erl
@@ -1,3 +1,27 @@
+%------------------------------------------------------------------------------
+% Módulo:       ordenar_tuplas
+% Propósito:    Ordenar tuplas
+% 
+% Autor:        Erick Corrales
+% Fecha:        18 de septiembre de 2024
+% Versión:      1.0
+%
+% Descripción: 
+% Aprender a oordenar tuplas
+%
+% Dependencias:
+% Ninguna.
+%
+% Ejemplo de Uso:
+% 1. Compilar el módulo: 
+%    c(ordenar_tuplas).
+% 2. Llamar a la función:
+%    ordenar_tuplas:main().
+%
+% Historial de Cambios:
+% 12/09/2024 - 1.0 - Creación del módulo con función básica `main/0` y `factorial/1`.
+%
+%------------------------------------------------------------------------------
 
 -module(ordenar_tuplas).
 -export([main/0, sort/1]).
