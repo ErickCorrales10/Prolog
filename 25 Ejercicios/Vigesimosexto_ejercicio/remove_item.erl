46ec1820c14fcb85dcc8386f80a7ab278106f7c4
Erick Roberto Corrales Quintero <143043585+ErickCorrales10@users.noreply.github.com>
2024-09-19 11:32:14 -0700

    Update remove_item.erl

--- 25 Ejercicios/Vigesimosexto_ejercicio/remove_item.erl
+++ 25 Ejercicios/Vigesimosexto_ejercicio/remove_item.erl
@@ -1,3 +1,28 @@
+%------------------------------------------------------------------------------
+% Módulo:       remove_item
+% Propósito:    Elimina un elemento de una lista.
+% 
+% Autor:        Erick Corrales
+% Fecha:        18 de septiembre de 2024
+% Versión:      1.0
+%
+% Descripción: 
+% Aprender a utilizar las herramientas de erlang para eliminar elementos de listas.
+%
+% Dependencias:
+% Ninguna.
+%
+% Ejemplo de Uso:
+% 1. Compilar el módulo: 
+%    c(remove_item).
+% 2. Llamar a la función:
+%    remove_item:main().
+%
+% Historial de Cambios:
+% 12/09/2024 - 1.0 - Creación del módulo con función básica `main/0` y `remove_item/2`.
+%
+%------------------------------------------------------------------------------
+
 -module(remove_item).
 -export([main/0]).
 
