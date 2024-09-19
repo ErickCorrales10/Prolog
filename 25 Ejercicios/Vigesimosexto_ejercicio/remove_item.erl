f19ac9b86551f6952687f064c1f0131a61111b51
Erick Roberto Corrales Quintero <143043585+ErickCorrales10@users.noreply.github.com>
2024-09-19 12:01:22 -0700

    Update remove_item.erl

--- 25 Ejercicios/Vigesimosexto_ejercicio/remove_item.erl
+++ 25 Ejercicios/Vigesimosexto_ejercicio/remove_item.erl
@@ -30,7 +30,7 @@ main() ->
   Lista = [1, 2, 3, 4, 5],
   io:format("La lista es ~p~n", [Lista]),
   NuevaLista = remove_element(2, Lista),
-  io:format("La nueva lista depues de eliminar el elemento en la posicion 2 es: ~p~n", [NuevaLista)).
+  io:format("La nueva lista depues de eliminar el elemento en la posicion 2 es: ~p~n", [NuevaLista]).
   
 remove_element(Index, Items) ->
     {Left, [_|Right]} = lists:split(Index-1, Items),
